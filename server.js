d0abbb5831a14d786e6d9c95d453833782ea5999
nikbabchenko <nik.babchenko@gmail.com>
2018-01-07 13:36:31 +0200

    Adds http redirect

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ app.use(cors());
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-// const forceSSL = function () {
-//     return function (req, res, next) {
-//         if (req.headers['x-forwarded-proto'] !== 'https') {
-//             return res.redirect(
-//                 ['https://', req.get('Host'), req.url].join('')
-//             );
-//         }
-//         next();
-//     }
-// }
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'http') {
+            return res.redirect(
+                ['http://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
 // // Instruct the app
 // // to use the forceSSL
 // // middleware
-// app.use(forceSSL());
+app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
